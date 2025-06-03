b33f9b59fda4f02a5a700fb70b9a2b0290ccff16
musicafiem25 <musicafiem@gmail.com>
2025-06-04 02:05:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,5 +20,8 @@ mongoose
   .catch((err) => console.error("Mongo error:", err));
 app.use("/api/auth", require('./routes/main'));
 app.use("/api/spotify", require('./routes/spotifyRoutes'));
+app.get("/",(req,res)=>{
+      res.send("<h1>Welcome to Musica</h1>");
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
