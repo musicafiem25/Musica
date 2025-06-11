443692d4930708cdc6ec8b8d05e1241a6e64321b
musicafiem25 <musicafiem@gmail.com>
2025-06-11 22:43:27 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,38 @@
+// const mongoose = require('mongoose');
+
+// // Subdocument schema for music tracks
+// const TrackSchema = new mongoose.Schema({
+//   id: String,
+//   name: String,
+//   artists: [Object],
+//   album: Object,
+//   preview_url: String,
+//   release_date: String,
+// }, { _id: false });
+
+// // User schema
+// const UserSchema = new mongoose.Schema({
+//   name: String,
+//   email: { type: String, unique: true, required: true },
+//   password: { type: String }, // for email/password auth
+//   googleId: { type: String, unique: true, sparse: true }, // for Google OAuth
+//   avatar: {
+//     data: Buffer,
+//     contentType: String
+//   },
+//   likedMusic: [TrackSchema],
+//   pinnedMusic: [TrackSchema],
+//   recentlyPlayed: [TrackSchema],
+//   isAdmin: { type: Boolean, default: false } // 👈 Added this line
+// }, { timestamps: true });
+
+// // Prevent OverwriteModelError
+// module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
+
+
+
 const mongoose = require('mongoose');
 
-// Subdocument schema for music tracks
 const TrackSchema = new mongoose.Schema({
   id: String,
   name: String,
@@ -10,12 +42,11 @@ const TrackSchema = new mongoose.Schema({
   release_date: String,
 }, { _id: false });
 
-// User schema
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true, required: true },
-  password: { type: String }, // for email/password auth
-  googleId: { type: String, unique: true, sparse: true }, // for Google OAuth
+  password: { type: String },
+  googleId: { type: String, unique: true, sparse: true },
   avatar: {
     data: Buffer,
     contentType: String
@@ -23,8 +54,12 @@ const UserSchema = new mongoose.Schema({
   likedMusic: [TrackSchema],
   pinnedMusic: [TrackSchema],
   recentlyPlayed: [TrackSchema],
-  isAdmin: { type: Boolean, default: false } // 👈 Added this line
+  isAdmin: { type: Boolean, default: false }
 }, { timestamps: true });
 
-// Prevent OverwriteModelError
-module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
+
+const ManualUser = mongoose.models.ManualUser || mongoose.model('ManualUser', UserSchema, 'manual_user');
+const GoogleUser = mongoose.models.GoogleUser || mongoose.model('GoogleUser', UserSchema, 'google_user');
+
+module.exports = { ManualUser, GoogleUser };
+
