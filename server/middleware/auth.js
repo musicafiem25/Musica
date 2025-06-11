7a8549495bdc81d9138feb687b284461e985a19c
musicafiem25 <musicafiem@gmail.com>
2025-06-11 22:42:21 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,27 @@
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+
+// const auth = async (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = await User.findById(decoded.id);
+//     if (!req.user) return res.status(404).json({ error: "User not found" });
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ error: "Invalid token" });
+//   }
+// };
+
+
+
+// module.exports = auth;
+
+
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const { ManualUser, GoogleUser } = require("../models/User");
 
 const auth = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
@@ -7,14 +29,19 @@ const auth = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+
+    
+    const manualUser = await ManualUser.findById(decoded.id);
+    const googleUser = !manualUser ? await GoogleUser.findById(decoded.id) : null;
+
+    req.user = manualUser || googleUser;
     if (!req.user) return res.status(404).json({ error: "User not found" });
+
     next();
   } catch (err) {
     res.status(401).json({ error: "Invalid token" });
   }
 };
 
-
-
 module.exports = auth;
+
